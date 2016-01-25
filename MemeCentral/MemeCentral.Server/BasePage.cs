00d71bbe6b3e8867208c2cfee18e1d4bdee7147b
Ivcho <ivo_rankov@mail.bg>
2016-01-25 20:35:50 +0200

    Base page object

---
+++ MemeCentral/MemeCentral.Server/BasePage.cs
@@ -0,0 +1,9 @@
+﻿namespace MemeCentral.Server
+{
+    using System.Web.UI;
+
+    public class BasePage : Page
+    {
+        //DbContext will be inited here, all other pages will inherit and have it in this.db...
+    }
+}
